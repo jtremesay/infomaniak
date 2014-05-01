ad744e2671e444ee4ad579739289bf8f48efc7fe
jtremesaygues <jonathan.tremesaygues@gmail.com>
2014-05-01 14:34:04 +0200

    Ajout de people

---
+++ Infomaniak/People.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Infomaniak;
+
+
+/**
+ * Une personne
+ *
+ * Une personne possède un nom, un prénom et un identifiant
+ */
+class Student {
+    protected $_id = 0;
+    protected $_firstName = "";
+    protected $_lastName = "";
+
+
+    /**
+     * Récupère l'identifiant
+     *
+     * @return int L'identifiant
+     */
+    public function getId() {
+        return $this->_id;
+    }
+
+
+    /**
+     * Définit l'identifiant
+     *
+     * @param int $id L'identifiant
+     */
+    public function setId($id) {
+        if (!is_int($id)) {
+            trigger_error('setId expected Argument $id to be int', E_USER_WARNING);
+        }
+
+        $this->_id = $id;
+    }
+
+
+    /**
+     * Récupère le prénom
+     *
+     * @return string
+     */
+    public function getFirstName() {
+        return $this->_firstName;
+    }
+
+
+    /**
+     * Définit le prénom
+     *
+     * @param string $firstName Le prénom
+     */
+    public function setFirstName($firstName) {
+        if (!is_string($firstName)) {
+            trigger_error('setFirstName expected Argument $firstName to be string', E_USER_WARNING);
+        }
+
+        $this->_firstName = $firstName;
+    }
+
+
+    /**
+     * Récupère le nom
+     *
+     * @return string
+     */
+    public function getLastName() {
+        return $this->_lastName;
+    }
+
+
+    /**
+     * Définit le nom
+     *
+     * @param string $lastName Le nom
+     */
+    public function setLastName($lastName) {
+        if (!is_string($lastName)) {
+            trigger_error('setLastName expected Argument $lastName to be string', E_USER_WARNING);
+        }
+
+        $this->_lastName = $lastName;
+    }
+}
